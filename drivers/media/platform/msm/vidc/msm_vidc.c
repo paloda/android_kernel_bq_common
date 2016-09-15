40266457491fe590235859dbed94825679b65599
paloda <paloda09@gmail.com>
2016-09-15 09:09:15 +0200

    msm: vidc: update the check to ignore extradata plane in  dqbuf
    userptr may or may not be a null-value. Checking for this field
    to qualify a plane as extradata has no merit. Ignore that check
    
    Bug: 28747768
    Change-Id: I08d85ef462f09003aaa17f6ebc5d27de7083796f
    Signed-off-by: Praveen Chavan <pchavan@codeaurora.org>
    Signed-off-by: Praneeth Paladugu <ppaladug@codeaurora.org>
    
    (cherry picked from commit 8d4478e429895a6d9687af2f00b5b4de21c32cfc)

--- drivers/media/platform/msm/vidc/msm_vidc.c
+++ drivers/media/platform/msm/vidc/msm_vidc.c
@@ -259,8 +259,11 @@ struct buffer_info *get_registered_buf(struct msm_vidc_inst *inst,
 	list_for_each_entry(temp, &inst->registeredbufs.list, list) {
 		for (i = 0; (i < temp->num_planes)
 			&& (i < VIDEO_MAX_PLANES); i++) {
+			bool ion_hndl_matches = temp->handle[i] ?
+				msm_smem_compare_buffers(inst->mem_client, fd,
+				temp->handle[i]->smem_priv) : false;
 			if (temp &&
-				((fd == temp->fd[i]) ||
+				(ion_hndl_matches ||
 				(device_addr == temp->device_addr[i])) &&
 				(CONTAINS(temp->buff_off[i],
 				temp->size[i], buff_off)
@@ -985,8 +988,8 @@ int msm_vidc_dqbuf(void *instance, struct v4l2_buffer *b)
 		if (!inst->map_output_buffer)
 			continue;
 		if (EXTRADATA_IDX(b->length) &&
-			(i == EXTRADATA_IDX(b->length)) &&
-			!b->m.planes[i].m.userptr) {
+			i == EXTRADATA_IDX(b->length))
+		{
 			continue;
 		}
 		buffer_info = device_to_uvaddr(&inst->registeredbufs,
